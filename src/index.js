09c2ddb4fb14ca36fff01fc8acb532a9d7e94431
ultrasoft011 <ultrasoft.011@gmail.com>
2021-05-24 16:22:02 -0500

    prop key for the child

--- src/index.js
+++ src/index.js
@@ -8,17 +8,20 @@ import "./index.css";
 //Array of objects for books
 const books = [
   {
+    id: 1,
     img: "https://m.media-amazon.com/images/I/91asIC1fRwL._AC_UY218_.jpg",
     title:
       "Eloquent JavaScript, 3rd Edition: A Modern Introduction to Programming",
     author: "Marijn Haverbeke",
   },
   {
+    id: 2,
     img: "https://m.media-amazon.com/images/I/71viHxTLKgL._AC_UY218_.jpg",
     title: "JavaScript and JQuery: Interactive Front-End Web Development",
     author: "Jon Duckett"
   },
   {
+    id: 3,
     img: "https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
     title: "You Dont Know JS Yet: Get Started",
     author: "Kyle Simpson",
@@ -30,7 +33,7 @@ function BookList() {
     <section className="booklist">
       {books.map((book) => {
         return (
-            <Book book={book}></Book>
+            <Book key={book.id} book={book}></Book>
         )
       })}
     </section>
